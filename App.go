f170cb5d1606ea8000b4acfe6173aab11fa0d55d
Olivier Eggimann <oli@olinetz.ch>
2019-03-26 14:54:20 +0100

    error correcting 1

--- App.go
+++ App.go
@@ -1,5 +1,39 @@
 package main
 
+import (
+	"fmt"
+	"github.com/seekbat/ArticleDownloader/LinkScraper"
+	"regexp"
+)
+
+type Site struct {
+	ID    int    `json:"id"`    //ID for Internal Use
+	Name  string `json:"name"`  //Name of the Site
+	URL   string `json:"url"`   //to the Site
+	Regex string `json:"regex"` //to find te Links
+}
+
 func main() {
 
+	min := Site{
+		ID:    1,
+		Name:  "20min",
+		URL:   "https://www.20min.ch",
+		Regex: `\/([A-Za-z0-9-]{1,})([0-9]{1,}$)`,
+	}
+
+	var links []string
+	r, _ := regexp.Compile(min.Regex)
+	links = LinkScraper.LinkScraper(min.URL, r)
+
+	for _, link := range links {
+		fmt.Println(link)
+	}
+
+}
+
+func checkErr(e error) {
+	if e != nil {
+		panic(e)
+	}
 }
