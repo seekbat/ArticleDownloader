add1933eed3bc23852a3541ed140922538185540
Olivier Eggimann <oli@olinetz.ch>
2019-04-30 11:25:49 +0200

    added mongo

---
+++ database/database_test.go
@@ -0,0 +1,10 @@
+package database
+
+/*
+ * ==========================================
+ *  Title:
+ *  Project:   ArticleDownloader
+ * Author:    Olivier Eggimann
+ * Date:      30.Apr.2019
+ * ==========================================
+ */
