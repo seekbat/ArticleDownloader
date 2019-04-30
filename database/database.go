0763e3712604c2e7ea9de6c708d0748f4a289e57
Olivier Eggimann <oli@olinetz.ch>
2019-04-30 09:53:18 +0200

    Added Database Package

---
+++ database/database.go
@@ -0,0 +1,14 @@
+package database
+
+/*
+ * ==========================================
+ *  Title:
+ *  Project:   ArticleDownloader
+ * Author:    Olivier Eggimann
+ * Date:      29.Apr.2019
+ * ==========================================
+ */
+
+func AddLinksToDb(links []string) error{
+
+}
