7d89bccdd5dbeb08575510c1b74257fe9e1e9df0
Olivier Eggimann <oli@olinetz.ch>
2019-05-04 01:12:12 +0200

    typo

--- App.go
+++ App.go
@@ -2,9 +2,8 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"github.com/seekbat/ArticleDownloader/LinkScraper"
-	"github.com/seekbat/ArticleDownloader/database
+	"github.com/seekbat/ArticleDownloader/database"
 	"github.com/seekbat/ArticleDownloader/models"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
@@ -13,6 +12,7 @@ import (
 	"strconv"
 	"time"
 )
+
 var Sites []models.Site
 var linklists []models.LinkList
 
@@ -22,24 +22,21 @@ func main() {
 		Name:      "20min",
 		URL:       "https://www.20min.ch",
 		LinkRegex: `\/([A-Za-z0-9-]{1,})([0-9]{1,}$)`,
-		IDRegex: `([0-9]{1,}$)`,
+		IDRegex:   `([0-9]{1,}$)`,
 	}
 	db := database.NewDatabase("mongodb://localhost:27017", context.TODO())
 
-
-
 	r, _ := regexp.Compile(min.LinkRegex)
-	for _,site := range Sites {
+	for _, site := range Sites {
 		var articlelinks []models.ArticleLink
 		articlelinks = linkscraper.LinkScraper(site.URL, site.LinkRegex, site.IDRegex)
-		var linklist = models.LinkList{site.Name,articlelinks}
+		var linklist = models.LinkList{site.Name, articlelinks}
 		linklists = append(linklists, linklist)
 	}
 
-	for _,linklist := range linklists  {
+	for _, linklist := range linklists {
 		db.AddLinksToDb(linklist)
 	}
-
 
 }
 
